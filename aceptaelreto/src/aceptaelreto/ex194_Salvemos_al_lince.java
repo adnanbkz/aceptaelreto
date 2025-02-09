78e6f265af81b5872290ac1922cdeb831669de61
adnaneeuu@gmail.com <adnaneeuu@gmail.com>
2025-02-09 16:52:12 +0100

    Update ex194_Salvemos_al_lince.java

--- aceptaelreto/src/aceptaelreto/ex194_Salvemos_al_lince.java
+++ aceptaelreto/src/aceptaelreto/ex194_Salvemos_al_lince.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package aceptaelreto;
+import java.util.Scanner;
 
 /**
  *
  * @author AdnanPC
  */
 public class ex194_Salvemos_al_lince {
-    
+    Scanner sc = new Scanner(System.in);    
+     int numCarreteras = sc.nextInt();
+     int i = 0;
+
+     for( i < numCarreteras; i++;) {
+
+     }
+
+
 }
+
